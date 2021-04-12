e64a9ee2d6d7d4e101bad36562d9b0c48ca23767
txag2020 <Tapdal2020@gmail.com>
2021-04-12 14:40:01 -0500

    reset production client id and redirect uri

--- src/components/config.js
+++ src/components/config.js
@@ -1,12 +1,12 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
-// export const clientId = "80bd87927d7646ee80b09b1eda3a412f";
-// export const redirectUri = "http://tapdal2020.github.io/portfolio";
+export const clientId = "80bd87927d7646ee80b09b1eda3a412f";
+export const redirectUri = "http://tapdal2020.github.io/portfolio";
 
 // Uncomment for developer version of the site
-export const redirectUri = "http://localhost:3000/";
-export const clientId = "c967bf533ad241478798748a9760f7f3";
+// export const redirectUri = "http://localhost:3000/";
+// export const clientId = "c967bf533ad241478798748a9760f7f3";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
